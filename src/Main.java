e564fc292409e597df5ec2fe04811a941f8f3652
Alexander Siluyanov <antidot8@mail.ru>
2021-04-03 21:40:21 +0400

    Add files

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(10000);
+        System.out.println(miles);
+    }
+}
